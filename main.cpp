9ff77e4947b76fe2062066e352818649594b8605
lancees <lance.sander@gmail.com>
2020-02-01 16:00:47 -0600

    Renmaed function to maxArray

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <algorithm>    // std::max
 
 template <class ElementType>
-ElementType r(ElementType a[], int asize) {
+ElementType maxArray(ElementType a[], int asize) {
     if (asize == 1) {
         return a[0];
     } else {
@@ -19,20 +19,20 @@ ElementType r(ElementType a[], int asize) {
         for (int i =quotient; i < asize; i++) { // pickup remaining  elements
             righthalf[i-quotient] = a[i];
         }
-        return std::max(r(lefthalf, quotient), r(righthalf, asize-quotient));
+        return std::max(maxArray(lefthalf, quotient), maxArray(righthalf, asize-quotient));
     }
 }
 
 int main()
 {
     int myints[7] = {6,7,92, 8, 9, 10, 4};
-    std::cout << r(myints, sizeof(myints)/sizeof(myints[0])) << std::endl;
+    std::cout << maxArray(myints, sizeof(myints)/sizeof(myints[0])) << std::endl;
 
     std::string mystrings[6] = {"a", "b", "c", "z", "e", "k"};
-    std::cout << r(mystrings, sizeof(mystrings)/sizeof(mystrings[0])) << std::endl;
+    std::cout << maxArray(mystrings, sizeof(mystrings)/sizeof(mystrings[0])) << std::endl;
 
     double mydoubles[3] = {1.1, 3.4, 2.6};
-    std::cout << r(mydoubles, sizeof(mydoubles)/sizeof(mydoubles[0])) << std::endl;
+    std::cout << maxArray(mydoubles, sizeof(mydoubles)/sizeof(mydoubles[0])) << std::endl;
 
     // wanted to compare colors but unsure how to implement MAX custom to my Color
 }
